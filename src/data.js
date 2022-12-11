7b77fc9e2fbb21d4d8d63f645df95f2000a35c0c
ganesh <ganeshsadhashivam@outlook.com>
2022-12-11 14:57:08 +0530

    Birthday Project

---
+++ src/data.js
@@ -0,0 +1,30 @@
+export default [
+  {
+    id: 1,
+    name: "Ganesh",
+    age: 30,
+    image:
+      "https://media.istockphoto.com/id/1342247162/photo/handsome-middle-eastern-guy-businessman-posing-next-to-office-center.jpg?b=1&s=170667a&w=0&k=20&c=aFFf4WomNrkbLljIvafbk2ukDK4chb34HGjbqb2sQGs=",
+  },
+  {
+    id: 2,
+    name: "Alina",
+    age: 30,
+    image:
+      "https://images.unsplash.com/photo-1544005313-94ddf0286df2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8Z2VybWFuJTIwZ2lybHxlbnwwfHwwfHw%3D&w=1000&q=80",
+  },
+  {
+    id: 3,
+    name: "Andrea",
+    age: 30,
+    image:
+      "https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?cs=srgb&dl=pexels-andrea-piacquadio-733872.jpg&fm=jpg",
+  },
+  {
+    id: 4,
+    name: "Ankita",
+    age: 30,
+    image:
+      "https://media.istockphoto.com/id/540833996/photo/small-indian-girl-and-colourful-palm-painting-or-palm-printing.jpg?s=612x612&w=0&k=20&c=FehjCQGJnwY4PClfL58emwsB_NzLAY04z_C3R8BQAg8=",
+  },
+];
